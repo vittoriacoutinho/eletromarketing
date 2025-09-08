afc9230df8946cba291fb410953f0ed24ec32db8
Vittoria Emmanuele Coutinho <92343571+vittoriacoutinho@users.noreply.github.com>
2025-09-08 11:24:20 -0300

    Update novo.php

--- eletromarketing-main/novo.php
+++ eletromarketing-main/novo.php
@@ -1,25 +1,41 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 <head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Usuário</title>
+
+    <!-- CSS -->
     <link rel="stylesheet" href="/eletromarketing-main/css/novo.css">
 
+    <!-- Ícones -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
     <h1>Cadastro de Usuário</h1>
 
     <form action="inserir.php" method="POST">
         <label>
-            Nome:
+            Email:
             <input type="email" name="email" required>
         </label>
+        <br>
+
         <label>
-            Email:
+            Senha:
             <input type="password" name="senha" required>
         </label>
-        <button type="submit"><i class="fas fa-save"></i> Salvar</button>
+        <br>
+
+        <button type="submit">
+            <i class="fas fa-save"></i> Salvar
+        </button>
     </form>
 
     <br>
-    <a href="cadastro.php" class="voltar"><i class="fas fa-arrow-left"></i> Voltar para a lista</a>
+    <a href="cadastro.php" class="voltar">
+        <i class="fas fa-arrow-left"></i> Voltar para a lista
+    </a>
 </body>
 </html>
+       
+
